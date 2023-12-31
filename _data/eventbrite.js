f04fa9914489238ba84c4eaf920d861fa170e354
Chris Bracco <chris@cbracco.me>
2023-12-31 13:17:34 -0500

    Add a few @TODOs; Add ticket_classes to eventbrite API call

--- _data/eventbrite.js
+++ _data/eventbrite.js
@@ -1,8 +1,10 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
+// @TODO cache this on the server: https://www.11ty.dev/docs/quicktips/cache-api-requests/
 module.exports = async function() {
     try {
-        let url = "https://www.eventbriteapi.com/v3/organizations/1905653968833/events/?token=ZQJEXIDK2TX35TK3D5ZM";
+        // @TODO encrypt API key
+        let url = "https://www.eventbriteapi.com/v3/organizations/1905653968833/events/?token=ZQJEXIDK2TX35TK3D5ZM&expand=ticket_classes";
 
         let json = EleventyFetch(url, {
             duration: "1d",
