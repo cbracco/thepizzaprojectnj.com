2cb23300e51139ce65a140a2861d5762d4af5690
Chris Bracco <chris@cbracco.me>
2024-01-04 01:50:44 -0500

    Minor cleanup

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -2,12 +2,7 @@ module.exports = {
     title: 'Bracco Bread Co.',
     url: 'https://braccobreadco.com/',
     language: 'en',
-    description: 'Rustic breads and pizza pies homebaked in New Jersey by Chris Bracco. Currently doing local pickups, popups, and special events.',
-    author: {
-        name: 'Your Name Here',
-        email: 'youremailaddress@example.com',
-        url: 'https://example.com/about-me/',
-    },
+    description: 'Rustic breads and pizza pies homebaked in New Jersey. Local pickups, popups, special events, and more.',
     social: {
         instagram: "https://instagram.com/braccobreadco",
     },
