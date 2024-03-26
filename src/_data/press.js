cd7e73af6fe7a9912be761d2d740fd59ed45b1a6
Chris Bracco <chris@cbracco.me>
2024-03-26 09:51:11 -0400

    Update press

--- src/_data/press.js
+++ src/_data/press.js
@@ -3,6 +3,7 @@ module.exports = [
         title: 'Bracco Bread Co. Pizza Pop-Up At Aquila',
         url: 'https://thedigestonline.com/dining/bracco-bread-co-pizza-pop-up/',
         author: 'Peter Candia',
+        publication: 'NJ Digest',
         date: 'March 6, 2024',
     },
 ];
